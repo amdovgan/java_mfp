e0c3cdcae0eafaf90050f3d30717ff0aa3afc8b3
Andrey Dovgan <adovgan@ro.ru>
2018-08-08 23:44:14 +0300

    Вычисляем площадь квадрата

--- sandbox/src/main/java/ru/mfp/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/mfp/sandbox/MyFirstProgramm.java
@@ -3,8 +3,21 @@ package ru.mfp.sandbox;
 public class MyFirstProgramm {
 
   public static void main(String [] args) {	
-    System.out.println("Hello, World");
+    hello("Andrey");
+    hello("user");
+    hello("world");
+
+    double len = 3;
+    System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
   }
-	
+
+  public static void hello(String nobody) {
+    System.out.println("Hello, " + nobody + "!");
+  }
+
+  public static double area(double side) {
+    return side*side;
+  }
 } 
 
