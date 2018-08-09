26db712a8c96df1826e7142ee1867ab3888426d9
Andrey Dovgan <adovgan@ro.ru>
2018-08-09 23:13:19 +0300

    Пункты 1, 2, 3 задания № 2

--- sandbox/src/main/java/ru/mfp/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/mfp/sandbox/MyFirstProgramm.java
@@ -15,6 +15,20 @@ public class MyFirstProgramm {
     Rectangle re = new Rectangle(4,5);
     System.out.println("Площадь прямоугольника со сторонами " + re.a + " $ " + re.b + " = " + re.area());
 
+    Point p1 = new Point();
+    Point p2 = new Point();
+    p1.x = 3;
+    p1.y = 4;
+    p2.x = 8;
+    p2.y = 7;
+    System.out.println("Расстояние между точкой " + p1.x + " " + p1.y +" и " + " точкой " + p2.x + " " + p2.y + " равно " + distance(p1,p2));
+
+
+
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(((p2.x-p1.x)*(p2.x-p1.x))+((p2.y-p1.y)*(p2.y-p1.y)));
   }
 
   public static void hello(String nobody) {
---
+++ sandbox/src/main/java/ru/mfp/sandbox/Point.java
@@ -0,0 +1,8 @@
+package ru.mfp.sandbox;
+
+public class Point {
+
+  public double x;
+  public double y;
+
+}
