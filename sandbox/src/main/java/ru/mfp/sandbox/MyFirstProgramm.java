2a6a8c84c3da9d3be387ae2d0571a083184d20e8
Andrey Dovgan <adovgan@ro.ru>
2018-08-09 13:43:02 +0300

    Еще немного функций

--- sandbox/src/main/java/ru/mfp/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/mfp/sandbox/MyFirstProgramm.java
@@ -8,7 +8,12 @@ public class MyFirstProgramm {
     hello("world");
 
     double len = 3;
+    double a = 4;
+    double b = 5;
+
     System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+    System.out.println("Площадь прямоугольника со сторонами " + a + " $ " + b + " = " + area(a,b));
 
   }
 
@@ -16,8 +21,14 @@ public class MyFirstProgramm {
     System.out.println("Hello, " + nobody + "!");
   }
 
+
   public static double area(double side) {
-    return side*side;
+    return side * side;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+
   }
 } 
 
