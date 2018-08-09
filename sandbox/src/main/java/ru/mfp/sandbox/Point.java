411e63383be3c2bc68482df7ba2eccf4dddf740d
Andrey Dovgan <adovgan@ro.ru>
2018-08-09 23:35:14 +0300

    Конструктор

--- sandbox/src/main/java/ru/mfp/sandbox/Point.java
+++ sandbox/src/main/java/ru/mfp/sandbox/Point.java
@@ -5,9 +5,9 @@ public class Point {
   public double x;
   public double y;
 
-  public Point (double xdot, double ydot) {
-    x = xdot;
-    y = ydot;
+  public Point (double x, double y) {
+    this.x = x;
+    this.y = y;
   }
 
 }
