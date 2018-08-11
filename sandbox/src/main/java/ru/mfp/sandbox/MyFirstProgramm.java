2b9443b7b8d922dee0aed0349d9b620e38499450
Andrey Dovgan <adovgan@ro.ru>
2018-08-11 16:44:03 +0300

    Пункт 4 задания № 2

--- sandbox/src/main/java/ru/mfp/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/mfp/sandbox/MyFirstProgramm.java
@@ -3,6 +3,7 @@ package ru.mfp.sandbox;
 public class MyFirstProgramm {
 
   public static void main(String [] args) {	
+/*
     hello("Andrey");
     hello("user");
     hello("world");
@@ -14,22 +15,28 @@ public class MyFirstProgramm {
 
     Rectangle re = new Rectangle(4,5);
     System.out.println("Площадь прямоугольника со сторонами " + re.a + " $ " + re.b + " = " + re.area());
+*/
 
     Point p1 = new Point(3,4);
     Point p2 = new Point(8,7);
 
-    System.out.println("Расстояние между точкой " + p1.x + " " + p1.y +" и " + " точкой " + p2.x + " " + p2.y + " равно " + distance(p1,p2));
+    //выводит результат из метода distance(double х, double у) который на вход принимает атрибуты класса
+    System.out.println("Расстояние между точкой " + p1.x + " " + p1.y +" и " + " точкой " + p2.x + " " + p2.y + " равно " + p1.distance(p2.x,p2.y));
 
+    //выводит результат из метода distance(Point p) который на вход принимает объект
+    System.out.println("Расстояние между точками равно " + p1.distance(p2));
 
   }
-
+/*
   public static double distance(Point p1, Point p2) {
     return Math.sqrt(((p2.x-p1.x)*(p2.x-p1.x))+((p2.y-p1.y)*(p2.y-p1.y)));
   }
-
+*/
+/*
   public static void hello(String nobody) {
     System.out.println("Hello, " + nobody + "!");
   }
+*/
 
 }
 
--- sandbox/src/main/java/ru/mfp/sandbox/Point.java
+++ sandbox/src/main/java/ru/mfp/sandbox/Point.java
@@ -10,4 +10,12 @@ public class Point {
     this.y = y;
   }
 
+  public double distance(double x, double y) {
+    return Math.sqrt(((this.x-x)*(this.x-x))+((this.y-y)*(this.y-y)));
+  }
+
+  public double distance(Point p) {
+    return distance(p.x, p.y);
+  }
+
 }
