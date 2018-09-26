8beddb432ab4b72cdaa8b5ada3f553e233605a67
Andrey Dovgan <adovgan@ro.ru>
2018-09-27 01:27:30 +0300

    правки конфига

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -1,9 +1,13 @@
 <?php
 $g_hostname = 'localhost';
-$g_db_type = 'mysql';
+$g_db_type = 'mysqli';
 $g_database_name = 'bugtracker';
 $g_db_username = 'root';
 $g_db_password = '';
+
+$g_crypto_master_salt = 'q1w2e3r4t5y6a7s8d9f0g';
+
 $g_signup_use_captcha = OFF;
-$g_phpMailer_method = PHP_MAILER_METHOD_SMTP;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
 $g_smtp_host = 'localhost';
+?>
