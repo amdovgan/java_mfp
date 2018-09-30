ff64b4d8a01c63c75fd0bcb29c3244e99725ccca
Andrey Dovgan <adovgan@ro.ru>
2018-10-01 01:02:20 +0300

    Задание №19

--- mantis-tests/src/test/java/ru/stqa/mfp/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/mfp/mantis/tests/SoapTests.java
@@ -16,6 +16,8 @@ public class SoapTests extends TestBase{
 
   @Test
   public void testGetProjects() throws MalformedURLException, ServiceException, RemoteException {
+    skipIfNotFixed(1);
+    System.out.println(1);
     Set<Project> projects = app.soap().getProjects();
     System.out.println(projects.size());
     for (Project project : projects) {
@@ -25,11 +27,18 @@ public class SoapTests extends TestBase{
 
   @Test
   public void testCreateIssue() throws RemoteException, ServiceException, MalformedURLException {
+    skipIfNotFixed(2);
+    System.out.println(2);
     Set<Project> projects = app.soap().getProjects();
     Issue issue = new Issue().withSummary("Test issue")
             .withDescription("Test issue Description").withProject(projects.iterator().next());
     Issue created = app.soap().addIssue(issue);
     assertEquals(issue.getSummary(), created.getSummary());
+  }
 
+  @Test
+  public void testIssueStatus() throws MalformedURLException, ServiceException, RemoteException{
+    skipIfNotFixed(3);
+    System.out.println(3);
   }
 }
--- mantis-tests/src/test/java/ru/stqa/mfp/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/mfp/mantis/tests/TestBase.java
@@ -1,12 +1,19 @@
 package ru.stqa.mfp.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.mfp.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 public class TestBase {
 
@@ -27,5 +34,18 @@ public class TestBase {
     app.stop();
   }
 
+  boolean isIssueOpen(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+    MantisConnectPortType mc = app.soap().getMantisConnect();
+    IssueData issue = mc.mc_issue_get(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"), BigInteger.valueOf(issueId));
+    if ((issue.getStatus().getName().equals("resolved"))||(issue.getStatus().getName().equals("closed"))) {
+      return false;
+    } else return true;
+  }
+
+  public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
 }
 
